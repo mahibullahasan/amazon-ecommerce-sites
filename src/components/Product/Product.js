9c7533840174a55a1e962b38af410adbe9a1b768
mahibullahasan <mahibullah15-9411@diu.edu.bd>
2022-10-30 12:56:24 +0600

    add image and title dynamically

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    const {img, name} = props.product;
-    return (
-        <div className='product'>
-            <div className='img-container'>
-                <img src={img} alt="" />
-            </div>
-            <div className='product-container'>
-                 <h4>{name}</h4>                     
-            </div>
-            
-        </div>
-    );
+  console.log(props.product);
+  const { img, name, seller, price, stock } = props.product;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h4 className="product-name">{name}</h4>
+        <br />
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>
+          <small>${price}</small>
+        </p>
+        <p>
+          <small>only {stock} left in stock - order soon</small>
+        </p>
+        <button>add to cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
-import fakeData from '../../fakeData';
-import './Shop.css'
-import Product from '../Product/Product';
+import React, { useState } from "react";
+import fakeData from "../../fakeData";
+import "./Shop.css";
+import Product from "../Product/Product";
 
-const Shop = () => { 
-    const first10 = fakeData.slice(0,10);
-    const [products, setProducts] = useState(first10);
-    
-    
-    return (
-        <div className='shop-container'>
-            <div className='product-container'>
-            
-            {
-                products.map(pd => <Product product={pd}></Product>)
-            }
-            
+const Shop = () => {
+  const first10 = fakeData.slice(0, 10);
+  const [products, setProducts] = useState(first10);
 
-            </div>
-            <div className='cart-container'>
-                <h4>Order Summary</h4>
-                <h5>Item Ordered: 0</h5>
-
-            </div>
-            
-        </div>
-
-    );
-    
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((pd) => (
+          <Product product={pd}></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>Order Summary</h4>
+        <h5>Item Ordered: 0</h5>
+      </div>
+    </div>
+  );
 };
 export default Shop;
